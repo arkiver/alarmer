9b5554483d0491d35314f8a8028fb46decbca659
Akshay Khole <akshay@reversehack.in>
2015-01-11 15:49:55 -0800

    Simple working alarm

---
+++ app/src/main/java/apps/fun/akshays/alarmer/AlarmReceiver.java
@@ -0,0 +1,38 @@
+package apps.fun.akshays.alarmer;
+
+/**
+ * Created by akshay on 1/11/15.
+ */
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.content.WakefulBroadcastReceiver;
+
+public class AlarmReceiver extends WakefulBroadcastReceiver{
+    public void onReceive(final Context context, Intent intent){
+        // Wil update the UI with message
+        MainActivity inst = MainActivity.instance();
+        inst.setAlarmText("Time to wake up!");
+
+        Uri alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+        if(alarmUri == null){
+            alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        }
+        Ringtone ringtone = RingtoneManager.getRingtone(context, alarmUri);
+
+        ringtone.play();
+
+        // will send a notification message
+
+        ComponentName comp = new ComponentName(context.getPackageName(),
+                AlarmService.class.getName());
+        startWakefulService(context, intent.setComponent(comp));
+
+        setResultCode(Activity.RESULT_OK);
+    }
+}
--- app/src/main/java/apps/fun/akshays/alarmer/MainActivity.java
+++ app/src/main/java/apps/fun/akshays/alarmer/MainActivity.java
@@ -4,36 +4,64 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.ToggleButton;
+import java.util.Calendar;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+
+    AlarmManager alarmManager;
+    private PendingIntent pendingIntent;
+    private TimePicker alarmTimePicker;
+    private static MainActivity inst;
+    private TextView alarmTextView;
+
+    public static MainActivity instance(){
+        return inst;
+    }
+
+    public void onStart(){
+        super.onStart();
+        inst = this;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        alarmTimePicker = (TimePicker) findViewById(R.id.alarmTimePicker);
+        alarmTextView = (TextView) findViewById(R.id.alarmText);
+        alarmManager = (AlarmManager ) getSystemService(ALARM_SERVICE);
     }
+    public void onToggleClicked(View view){
+        if (((ToggleButton) view).isChecked()){
+            Log.d("My activity", "Alarm is switched on");
+
+            Calendar calendar = Calendar.getInstance();
+            calendar.set(Calendar.HOUR_OF_DAY, alarmTimePicker.getCurrentHour());
+            calendar.set(Calendar.MINUTE, alarmTimePicker.getCurrentMinute());
+
+            Intent myIntent = new Intent(MainActivity.this, AlarmReceiver.class);
+            pendingIntent = PendingIntent.getBroadcast(MainActivity.this, 0, myIntent, 0);
+            alarmManager.set(AlarmManager.RTC, calendar.getTimeInMillis(), pendingIntent);
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        } else{
+            alarmManager.cancel(pendingIntent);
+            setAlarmText("");
+            Log.d("My activity", "Alarm off");
         }
-
-        return super.onOptionsItemSelected(item);
+    }
+    public void setAlarmText(String alarmText){
+        alarmTextView.setText(alarmText);
     }
 }
